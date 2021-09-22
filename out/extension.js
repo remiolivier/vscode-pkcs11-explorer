5a652c108e79debec3f89a1e0a1192c4dcc81910
Remi Olivier <remi_8@hotmail.com>
2021-09-22 10:41:42 -0700

    remove out files

--- out/extension.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.activate = void 0;
-const pkcs11View_1 = require("./view/pkcs11View");
-function activate(context) {
-    new pkcs11View_1.Pkcs11View(context);
-}
-exports.activate = activate;
-//# sourceMappingURL=extension.js.map
